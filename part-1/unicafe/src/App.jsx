85e4a6ba5bbf8a3c023266ab403a261117dca59e
Dlorodev <dlorodev@gmail.com>
2024-09-04 18:47:47 -0500

    Add the exercises 1.7 and 1.8

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -7,17 +7,38 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   const handleGood = () => {
-    setGood(good + 1);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setAll(updatedGood + neutral + bad);
+    setAverage(
+      (updatedGood * 1 + neutral * 0 + bad * -1) / (updatedGood + neutral + bad)
+    );
+    setPositive(updatedGood / (updatedGood + neutral + bad) * 100);
   };
 
   const handleNeutral = () => {
-    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setAll(good + updatedNeutral + bad);
+    setAverage(
+      (good * 1 + updatedNeutral * 0 + bad * -1) / (good + updatedNeutral + bad)
+    );
+    setPositive(good / (good + updatedNeutral + bad) * 100);
   };
 
   const handleBad = () => {
-    setBad(bad + 1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setAll(good + neutral + updatedBad);
+    setAverage(
+      (good * 1 + neutral * 0 + updatedBad * -1) / (good + neutral + updatedBad)
+    );
+    setPositive(good / (good + neutral + updatedBad) * 100);
   };
 
   return (
@@ -30,6 +51,9 @@ function App() {
       <Statistics text="good" value={good} />
       <Statistics text="neutral" value={neutral} />
       <Statistics text="bad" value={bad} />
+      <Statistics text="all" value={all} />
+      <Statistics text="average" value={average} />
+      <Statistics text="positive" value={positive} unit="%" />
     </>
   );
 }
--- part-1/unicafe/src/Statistics.jsx
+++ part-1/unicafe/src/Statistics.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-const Statistics = ({ text, value }) => {
+const Statistics = ({ text, value, unit }) => {
   return (
     <>
       <p>
-        {text} {value}
+        {text} {value} {unit}
       </p>
     </>
   );
