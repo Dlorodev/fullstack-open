3bb307d9f4b2032350154695bcaef7e2e3705ed1
Dlorodev <dlorodev@gmail.com>
2025-08-19 22:59:59 -0500

    add the exercise 4.2

--- part-4/bloglist/index.js
+++ part-4/bloglist/index.js
@@ -1,44 +1,8 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const app = require('./app')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const user = process.env.DB_USER
-const pass = process.env.DB_PASS
-const mongoUrl = `mongodb+srv://${user}:${pass}@clusterfso.g8sanm2.mongodb.net/blogsApp?retryWrites=true&w=majority&appName=ClusterFSO`
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
