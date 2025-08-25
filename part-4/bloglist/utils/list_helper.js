d56b2166d7e9f4e722eba75da7abc78f8d6d839f
Dlorodev <dlorodev@gmail.com>
2025-08-25 16:16:13 -0500

    add the exercise 4.4

--- part-4/bloglist/utils/list_helper.js
+++ part-4/bloglist/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+    const reducer = (accum, actualBlog) => {
+        const likes = actualBlog.likes
+        return accum + likes
+    }
+
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
