a389903fcdc5bc83f166aea885ad1e0763e695c1
Dlorodev <dlorodev@gmail.com>
2025-08-26 12:09:22 -0500

    add the exercise 4.5*

--- part-4/bloglist/utils/list_helper.js
+++ part-4/bloglist/utils/list_helper.js
@@ -4,15 +4,32 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
 
-    const reducer = (accum, actualBlog) => {
-        const likes = actualBlog.likes
-        return accum + likes
-    }
+  const reducer = (accum, actualBlog) => {
+    const likes = actualBlog.likes
+    return accum + likes
+  }
 
-    return blogs.reduce(reducer, 0)
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+
+  if(blogs.length === 0){
+    return {}
+  }
+  
+  const likes = blogs.map(b => {
+    return b.likes
+  })
+  const sortedLikes = likes.sort((a, b) => a - b)
+  const mostLikes = sortedLikes[sortedLikes.length - 1]
+  const indexOfFavorite = blogs.findIndex(blog => blog.likes === mostLikes)
+
+  return blogs[indexOfFavorite]
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
